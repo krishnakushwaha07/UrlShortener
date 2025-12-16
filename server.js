642566683ed0461c11a56c687e3e857ea649162e
krishna kushwaha <krishnakushwaha2248@gmail.com>
2025-12-16 18:16:07 +0530

    fixing issue in server

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 import express from "express";
-import path from "path"
 import { shortenerRoutes } from "./routes/shortener.routes.js";
-import { fileURLToPath } from "url";
 
 const app = express();
 app.set("view engine","ejs")
 
 // serving html and css file through express.static() middleware.
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static("public"));
 
 
 app.use("/",shortenerRoutes);
