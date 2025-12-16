deaba4a57cc3e3a98da282b9ec9caf07f0186a7a
krishna kushwaha <krishnakushwaha2248@gmail.com>
2025-12-16 17:27:51 +0530

    fixing middleware path.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { fileURLToPath } from "url";
 import path from "path"
 import { shortenerRoutes } from "./routes/shortener.routes.js";
 import { fileURLToPath } from "url";
